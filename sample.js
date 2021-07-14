6d0ff054811566d43be542e78306b5a9e86244f6
RyutaroMaeda <ryutaromaeda83@gmail.com>
2021-07-14 13:01:46 +0900

    [finish]js-2.5

---
+++ sample.js
@@ -0,0 +1,40 @@
+// const nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
+
+// var str = "webcamp"
+
+// function foo(){
+//   console.log(str)
+//   var y ="hello"
+// }
+
+// foo()
+// console.log(y)
+
+// function foo(){
+//   let x = "webcamp"
+// {
+//     let y = "hell webcamp"
+// }
+// console.log(x)
+// console.log(y)
+// }
+
+// foo()
+
+// for(let i=0;i<10;i++){
+//   console.log(i)
+// }
+// console.log(i)
+
+var str = "webcamp"
+
+function foo(){
+  console.log(str)
+  var str = "dmm webcamp"
+  console.log(str)
+}
+
+foo()
